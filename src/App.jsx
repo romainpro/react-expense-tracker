6ddc8d4575e13337a5d5f5bf30d5c6d2394f3bfd
romain.casagrande <romain.casagrande.pro@gamil.com>
2023-09-13 09:18:05 +0200

    Add-income-input-function-onChange

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ExpenseInput } from "./containers/ExpenseInput/ExpenseInput";
 import { List } from "components/List/List";
 import s from "./style.module.css";
 import { useSelector } from "react-redux";
+import { IncomeInput } from "components/IncomeInput/IncomeInput";
 
 export function App() {
   const expenseList = useSelector((store) =>store.EXPENSE.expenseList);
@@ -13,7 +14,7 @@ export function App() {
           Logo
         </div>
         <div className={`col-9 ${s.income_input}`}>
-          Income input
+          <IncomeInput/>
         </div>
       </div>
       <div className={`row ${s.workspace}`}>
--- src/components/IncomeInput/IncomeInput.jsx
+++ src/components/IncomeInput/IncomeInput.jsx
@@ -1,11 +1,15 @@
 import s from "./style.module.css";
 
 export function IncomeInput(props) {
+  function updateIcome(e){
+    const income =e.target.value
+    console.log(income)
+  }
   return (
     <div className="row justify-content-center mb-2">
       <div className={`col-6 ${s.label}`}>Income</div>
       <div className="col-6">
-        <input type="number" className="form-control" placeholder="Ex: 3000" />
+        <input onChange={updateIcome} type="number" className="form-control" placeholder="Ex: 3000" />
       </div>
     </div>
   );
